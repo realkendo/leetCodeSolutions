e6289ebf6a3a315eebbba513e49bfacd40de9fc8
realkendo <kennethistifanus1@gmail.com>
2025-12-30 15:48:51 +0100

    Two sum first solution

---
+++ js/_1_twoSum.js
@@ -0,0 +1,33 @@
+/*
+
+1. Two Sum - Easy
+
+Hint
+Given an array of integers nums and an integer target,
+return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution,
+and you may not use the same element twice.
+
+You can return the answer in any order.
+
+*/
+
+//two sum function
+const twoSum = (myArray, myTarget) => {
+  // looping through the array for result
+  for (let i = 0; i < myArray.length; i++) {
+    for (let j = i + 1; j < myArray.length; j++) {
+      if (myArray[i] + myArray[j] === myTarget) {
+        return [i, j];
+      }
+    }
+  }
+  return;
+};
+
+// assigning values for test cases
+const arr = [2, 3, 4, 5, 6];
+const tar = 7;
+console.log(twoSum(arr, tar));
+
+// the above solution has a bigO complexity of O(n^2) because fo the loop inside of a loop
